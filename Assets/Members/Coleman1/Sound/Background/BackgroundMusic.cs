4be65e25a1ff9e9e87bd13869bc09f04d197a4dc
Coleman Ostach <colemandbest@gmail.com>
2023-04-12 22:28:59 -0400

    Commit for Background Music
    Includes:
    
    Background Music Prefab.
    
    BackgroundMusic script. (To swap clips between scenes and Fade In and Fade Out clips)

--- Assets/Members/Coleman1/Sound/Background/BackgroundMusic.cs
+++ Assets/Members/Coleman1/Sound/Background/BackgroundMusic.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Xml.Serialization;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 namespace OurGame
 {
     public class BackgroundMusic : MonoBehaviour
     {
+        #region Singletone Reference to the Music Object
+
         private static BackgroundMusic instance = null;
 
         public static BackgroundMusic Instance
@@ -24,6 +31,76 @@ namespace OurGame
                 instance = this;
 
             DontDestroyOnLoad(this.gameObject);
+        }
+
+        #endregion
+
+        private AudioSource track1, track2; //Need two audio sources to fade in and fade out of them
+        private bool isPlayingTrack1;
+
+        [SerializeField] private AudioClip defaultTrack;
+
+        [SerializeField] private AudioMixerGroup musicGroup;
+
+        private int sceneIndex;
+
+        private void Start()
+        {
+            track1 = gameObject.AddComponent<AudioSource>();
+            track2 = gameObject.AddComponent<AudioSource>();
+
+            track1.outputAudioMixerGroup = musicGroup;
+            track2.outputAudioMixerGroup = musicGroup;
+
+            isPlayingTrack1 = true;
+
+            SwapTrack(defaultTrack);
+        }
+
+        public void SwapTrack(AudioClip audioClip)
+        {
+            StopAllCoroutines();
+
+            StartCoroutine(FadeTrack(audioClip));
+
+            isPlayingTrack1 = !isPlayingTrack1;
+        }
+
+        private IEnumerator FadeTrack(AudioClip audioClip)
+        {
+            float timeToFade = 0.25f;
+            float timeElapsed = 0;
+
+            if (isPlayingTrack1)
+            {
+                track2.clip = audioClip;
+                track2.Play();
+
+                while(timeElapsed < timeToFade)
+                {
+                    track2.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                    track1.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                    timeElapsed += Time.deltaTime;
+                    yield return null;
+                }
+
+                track1.Stop();
+            }
+            else
+            {
+                track1.clip = audioClip;
+                track1.Play(); 
+                
+                while (timeElapsed < timeToFade)
+                {
+                    track1.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                    track2.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                    timeElapsed += Time.deltaTime;
+                    yield return null;
+                }
+
+                track2.Stop();
+            }
         }
     }
 }
